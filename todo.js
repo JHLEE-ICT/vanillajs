9f49417cf53d8799e811eaa0e3115638c8146e1d
JHLEE-ICT <jhlee.ict@gmail.com>
2020-02-05 12:23:55 +0900

    seiner project part one

--- todo.js
+++ todo.js
@@ -1,65 +1,151 @@
 const toDoForm = document.querySelector(".js-toDoForm"),
   toDoInput = toDoForm.querySelector("input"),
-  toDoList = document.querySelector(".js-toDoList");
+  pendingList = document.querySelector(".pendingList"),
+  finishedList = document.querySelector(".finishedList");
 
-const TODOS_LS = "toDos";
+const PENDING_LS = "PENDING",
+  FINISHED_LS = "FINISHED";
 
-let toDos = [];
+let pendArr = [],
+  finArr = [];
 
-function deleteToDo(event) {
+function saveFinItem() {
+  localStorage.setItem(FINISHED_LS, JSON.stringify(finArr));
+}
+
+function savePendItem() {
+  localStorage.setItem(PENDING_LS, JSON.stringify(pendArr));
+}
+
+function addPendList(event) {
+  const btn = event.target;
+  const pendLi = btn.parentNode;
+  const text = pendLi.childNodes[0].innerText;
+  //Finished List에서 삭제
+  finishedList.removeChild(pendLi);
+  const cleanFinArr = finArr.filter(function(toDo) {
+    return toDo.id !== parseInt(pendLi.id, 10);
+  });
+  finArr = cleanFinArr;
+  saveFinItem();
+  //PendingList Html 추가
+  paintPendingList(text);
+}
+
+function delFinList(event) {
   const btn = event.target;
   const li = btn.parentNode;
-  toDoList.removeChild(li);
-  const cleanToDos = toDos.filter(function(toDo) {
-    return toDo.id !== parseInt(li.id);
+  finishedList.removeChild(li);
+  const cleanFinArr = finArr.filter(function(toDo) {
+    return toDo.id !== parseInt(li.id, 10);
   });
-  toDos = cleanToDos;
-  saveToDos();
+  finArr = cleanFinArr;
+  saveFinItem();
 }
 
-function saveToDos() {
-  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+function delPendList(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  pendingList.removeChild(li);
+  const cleanPendArr = pendArr.filter(function(toDo) {
+    return toDo.id !== parseInt(li.id, 10);
+  });
+  pendArr = cleanPendArr;
+  savePendItem();
 }
 
-function paintToDo(text) {
+function addFinishedList(event) {
+  const btn = event.target;
+  const pendLi = btn.parentNode;
+  const text = pendLi.childNodes[0].innerText;
+  //pending List에서의 삭제
+  pendingList.removeChild(pendLi);
+  const cleanPendArr = pendArr.filter(function(toDo) {
+    return toDo.id !== parseInt(pendLi.id, 10);
+  });
+  pendArr = cleanPendArr;
+  savePendItem();
+  //finished List에서의 html 추가
+  paintFinishedList(text);
+}
+
+function paintFinishedList(text) {
   const li = document.createElement("li");
-  const delBtn = document.createElement("button");
   const span = document.createElement("span");
-  const newId = toDos.length + 1;
-  delBtn.innerText = "❌";
-  delBtn.addEventListener("click", deleteToDo);
+  const delBtn = document.createElement("button");
+  const pendBtn = document.createElement("button");
+  const newId = finArr.length + 1;
   span.innerText = text;
-  li.appendChild(delBtn);
+  delBtn.innerText = "❌";
+  pendBtn.innerText = "⏩";
   li.appendChild(span);
+  li.appendChild(delBtn);
+  li.appendChild(pendBtn);
   li.id = newId;
-  toDoList.appendChild(li);
-  toDoObj = {
+  delBtn.addEventListener("click", delFinList);
+  pendBtn.addEventListener("click", addPendList);
+  finishedList.appendChild(li);
+  //localStorage에 추가
+  const finObj = {
     text: text,
     id: newId
   };
-  toDos.push(toDoObj);
-  saveToDos();
+  finArr.push(finObj);
+  saveFinItem();
+}
+
+function paintPendingList(text) {
+  //html에 추가
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  const delBtn = document.createElement("button");
+  const finBtn = document.createElement("button");
+  const newId = pendArr.length + 1;
+  span.innerText = text;
+  delBtn.innerText = "❌";
+  finBtn.innerText = "✅";
+  li.appendChild(span);
+  li.appendChild(delBtn);
+  li.appendChild(finBtn);
+  li.id = newId;
+  delBtn.addEventListener("click", delPendList);
+  finBtn.addEventListener("click", addFinishedList);
+  pendingList.appendChild(li);
+  //localStorage에 추가
+  const pendObj = {
+    text: text,
+    id: newId
+  };
+  pendArr.push(pendObj);
+  savePendItem();
 }
 
 function handleSubmit(event) {
   event.preventDefault();
-  const currentValue = toDoInput.value;
-  paintToDo(currentValue);
+  const task = toDoInput.value;
+  paintPendingList(task);
   toDoInput.value = "";
 }
 
-function loadToDos() {
-  const loadedToDos = localStorage.getItem(TODOS_LS);
-  if (loadedToDos !== null) {
-    const parsedToDos = JSON.parse(loadedToDos);
-    parsedToDos.forEach(function(toDo) {
-      paintToDo(toDo.text);
+function loadedList() {
+  const pList = localStorage.getItem(PENDING_LS);
+  const fList = localStorage.getItem(FINISHED_LS);
+  if (pList !== null) {
+    const parsePList = JSON.parse(pList, 10);
+    parsePList.forEach(function(toDo) {
+      paintPendingList(toDo.text);
+    });
+  }
+  if (fList !== null) {
+    const parseFList = JSON.parse(fList);
+    parseFList.forEach(function(toDo) {
+      paintFinishedList(toDo.text);
     });
   }
 }
 
 function init() {
-  loadToDos();
+  loadedList();
   toDoForm.addEventListener("submit", handleSubmit);
 }
 
