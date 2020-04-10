f3ae1bcc68bab7798d7a94cbaa10d51dc9f89432
JHLEE-ICT <jhlee.ict@gmail.com>
2020-04-11 00:48:36 +0900

    update clock

--- clock.js
+++ clock.js
@@ -1,14 +1,17 @@
 const clockContainer = document.querySelector(".js-clock"),
-  clockTitle = clockContainer.querySelector(".js-clockTitle");
+  clockTitle = clockContainer.querySelector(".js-clockTitle"),
+  jsHour = clockTitle.querySelector(".js-hour"),
+  jsMin = clockTitle.querySelector(".js-min"),
+  jsSec = clockTitle.querySelector(".js-sec");
 
 function getTime() {
   const date = new Date();
   const minutes = date.getMinutes();
   const hours = date.getHours();
   const seconds = date.getSeconds();
-  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
-    minutes < 10 ? `0${minutes}` : minutes
-  }:${seconds < 10 ? `0${seconds}` : seconds}`;
+  jsHour.innerHTML = `${hours < 10 ? `0${hours}` : hours}`;
+  jsMin.innerHTML = `${minutes < 10 ? `0${minutes}` : minutes}`;
+  jsSec.innerHTML = `${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
 function init() {
